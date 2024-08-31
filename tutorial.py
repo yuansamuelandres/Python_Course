0f00d20425c32dd05ee25ad94d609543ce175726
yuansamuelandres <yuandeslytherinandres@gmail.com>
2024-08-31 16:33:34 +0200

    Added the third set of Py code notes

--- tutorial.py
+++ tutorial.py
@@ -1,42 +1,46 @@
 # In the name of Allah
 
-import random
+name = "Yuan"
+print(isinstance(name, int))
 
-def get_choices ():          #indentation is extremely important
-    player_choice = input("Enter a choice (rock, paper, scissors): ")
-    options = ["rock", "paper", "scissors"]
-    computer_choice = random.choice(options)
-    choices = {"player": player_choice, "computer": computer_choice}
-    return choices
-choices = get_choices ()
-print(choices)
+def is_integer (name):
+    return True if isinstance(name, int) else False
+print(is_integer(name))
 
-def check_win (player, computer):
-    print(f"You chose {player}. Computer chose {computer}")
-    if player == computer:
-        print("It's a tie")
-        # get_choices()
-        # check_win(choices["player"], choices["computer"])
-    elif player == "rock":
-        if computer == "scissors":
-            return "Rock smashes scissors! You win!"
-        else:
-            return "Paper covers rock! You lose."
-    elif player == "paper":
-        if computer == "rock":
-            return "Paper covers rock! You win!"
-        else:
-            return "Scissors cuts paper! You lose."
-    elif player == "scissors":
-        if computer == "rock":
-            return "Rock smashes scissors! You lose."
-        else:
-            return "Scissors cuts paper! You win!"
-result = check_win(choices["player"], choices["computer"])
-print(result)
+""" 
+Data Types: isinstance( , )
+    str
+    int         
+    float      
+    complex
+    bool
+    list
+    tuple
+    range
+    dict
+    set
 
-""" Function Arguments
-    if Statements
-    f Strings
-    Accessing Dictionary Values
+Casting:
+    int(str)
+    float(int)
+
+Operators:
+    - **
+    - //      Floor Division
+    - <=
+    - >=
+    - and
+    - or    return the other that's not flase
+    - not
+    - Bitwise Operators:
+        &   binary AND
+        |   binary OR
+        ^   binary XOR
+        ~   binary NOT
+        <<  shift left
+        >>  shift right
+    
+    - is
+    - in
+    - Ternary Operator
 """
